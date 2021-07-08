be8f3f4944f6b03ece5ea4696c4152f2422e4eef
legui19 <59314957+legui19@users.noreply.github.com>
2021-07-07 21:31:06 -0300

    Estos archivos actualizados al 7.7.21

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import NavBar from "./components/NavBar/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>Hola Clientes de Anke & Bags</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>Anke&Bags</h1>
+            <ul>
+                <li>Inicio</li>
+                <li>¿Como Comprar?</li>
+                <li>Productos</li>
+                <li>Sobre Nosotros</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
